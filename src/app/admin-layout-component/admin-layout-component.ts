59bdc6cbd9c564b4d12beca9d04cf7f3e37bf34d
xltgui <borba.liver@gmail.com>
2025-11-12 22:00:07 -0300

    FEAT: adding admin layout component

---
+++ src/app/admin-layout-component/admin-layout-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-layout-component',
+  imports: [],
+  templateUrl: './admin-layout-component.html',
+  styleUrl: './admin-layout-component.scss'
+})
+export class AdminLayoutComponent {
+
+}
